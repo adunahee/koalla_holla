6f023980185ffd459277cba0d316882d35271b17
Nicholas Heilman <Nicholas.heilman@gmail.com>
2019-01-10 13:24:37 -0600

    feature-server-delete router.delete

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -14,5 +14,15 @@ const koalaRouter = express.Router();
 
 
 // DELETE
-
+//
+router.delete('/:id', (req, res) => {
+    console.log('Params',req.params); 
+    const queryText = `DELETE FROM "koala" WHERE "id" = $1`;
+    pool.query(queryText, [req.params.id]).then((result) => {
+        res.sendStatus(200);
+    }).catch((error) => {
+        console.log('Error in DELETE /koala', error);
+        res.sendStatus(500);
+    });
+});
 module.exports = koalaRouter;
