3436d8a5b657aa7b7aa0e0730d1b4bf2f5422b87
Erin Olson <erineolson15@gmail.com>
2019-01-10 13:48:55 -0600

    server updates

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -4,15 +4,25 @@ const koalaRouter = express.Router();
 // DB CONNECTION
 
 
-// GET
+// GET Erin
+router.get('/', (req, res) => {
+    let queryText = `SELECT * FROM "koalas";`
+    pool.query(queryText).then((result)=>{
+        console.log(result);
+        res.send(result.rows); //result.rows will be an Aray
+    }).catch((error)=> {
+        console.log(`Error in getKoalas ${error}`);
+        res.sendStatus(500);
+    });
+    console.log(`in GetKoals`);
+}); 
+
+// POST Anthony 
 
 
-// POST
+// PUT Tiana
 
 
-// PUT
-
-
-// DELETE
+// DELETE Nick 
 
 module.exports = koalaRouter;
