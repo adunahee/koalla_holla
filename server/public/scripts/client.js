d523b65cc0491120382f5fc4ec837960981f8c7e
Anthony D <dunahee@gmail.com>
2019-01-10 14:18:56 -0600

    grab input data and send to server

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,11 +6,11 @@ $( document ).ready( function(){
   setupClickListeners()
   // load existing koalas on page load
   getKoalas();
-
+  placeholderValues();
 }); // end doc ready
 
 function setupClickListeners() {
-  $( '#addButton' ).on( 'click', saveKoala); 
+  $( '#addButton' ).on( 'click', saveKoala, addKoala); 
 }
 
 function getKoalas(){
@@ -22,4 +22,31 @@ function getKoalas(){
 function saveKoala(){
   // ajax call to server to get koalas
  
+}
+
+function placeholderValues () {
+  $('#nameIn').val('Fred');
+  $('#ageIn').val(4);
+  $('#genderIn').val('M');
+  $('#readyForTransferIn').val('N');
+  $('#notesIn').val('Enjoys long walks on the beach');
+}
+
+function addKoala() {
+  $.ajax({
+    method: 'POST', 
+    url: '/koalas',
+    data: {
+      name: $('#nameIn').val(),
+      gender: $('#genderIn').val(),
+      age: $('#ageIn').val(),
+      ready_to_transfer: $('#readyForTransferIn').val(),
+      notes: $('#notesIn').val()
+    }
+  }).then((res)=>{
+    console.log(res);
+    getKoalas();
+  }).catch((error) => {
+    console.log('server error for ajax POST', error);
+  })
 }
