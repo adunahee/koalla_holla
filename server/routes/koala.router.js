764bcd12f3114e43d891c92ab407924471c75cb6
Anthony D <dunahee@gmail.com>
2019-01-10 13:43:04 -0600

    router has db connection and working post

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,14 +1,37 @@
 const express = require('express');
 const koalaRouter = express.Router();
 
-// DB CONNECTION
+const pg = require('pg');
+const Pool = pg.Pool; //class
 
+
+// DB CONNECTION
+const pool = new Pool({
+    database: 'koalla-holla', //the agreed upon name
+    host: 'localhost', // we all have our own on our comps
+    port: 5432, //default
+    max: 10, //to prevent overload
+    idleTimeoutMillis: 10000 //10 secs to not waste time
+});
 
 // GET
 
 
 // POST
-
+koalaRouter.post('/', (req, res) => {
+    const newKoala = req.body;
+    console.log(newKoala);
+    
+    let queryText = `INSERT INTO "koalas" ("name", "gender", "age", "ready_to_transfer", "notes")
+                     VALUES ($1, $2, $3, $4, $5);`;
+    pool.query(queryText, [newKoala.name, newKoala.gender, newKoala.age, newKoala.ready_to_transfer, newKoala.notes]).then((dbres) => {
+        console.log(dbres);
+        res.sendStatus(201);
+    }).catch((error) => {
+        console.log('error in POST /koala', error);
+        res.sendStatus(500);
+    })
+})
 
 // PUT
 
--- server/server.js
+++ server/server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
-const koalaRouter = require('./routes/koala.router')
+const koalaRouter = require('./routes/koala.router.js')
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('server/public'));
 
 // ROUTES
-app.use('/koalas', koalaRouter)
+app.use('/koalas', koalaRouter);
 
 // Start listening for requests on a specific port
 app.listen(PORT, () => {
