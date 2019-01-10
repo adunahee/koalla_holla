cc7204a7375c35426d14e5edf5e3731c5540f10b
Tiana Johnson <Tiana.Marie013@gmail.com>
2019-01-10 13:42:45 -0600

    PUT created, need to test

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -11,7 +11,14 @@ const koalaRouter = express.Router();
 
 
 // PUT
-
+router.put('/:id', (req,res) => {
+    const queryText = `UPDATE "koalas" SET "ready_to_transfer" = true;`;
+    pool.query(queryText, [req.params.id]).then((result) => {
+        res.sendStatus(200);
+    }).catch((error) => {
+        console.log('error into PUT "koalas" in "ready_to_transfer"');
+    });
+})
 
 // DELETE
 
